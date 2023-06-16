1b0ecee51f448dc04166e548c7de373236952da2
AndreeaC24 <119935685+AndreeaC24@users.noreply.github.com>
2023-06-16 19:46:26 +0300

    header1

---
+++ src/js/markup.js
@@ -0,0 +1,58 @@
+import { getGenres } from './fetchGenre'; 
+
+const nullPoster = `https://tacm.com/wp-content/uploads/2018/01/no-image-available.jpeg`;
+
+const handleResponse = (data, isPopular = false) => {
+  if (!data.results) {
+    console.error('Invalid API response');
+    return;
+  }
+
+  const galleryElement = getGalleryElement();
+  if (!galleryElement) {
+    return;
+  }
+
+  const markup = data.results
+    .map((result, index) => {
+      if (isPopular) {
+        return markupGalleryItem(result, index);
+      } else {
+        return markupGalleryItem(result, index);
+      }
+    })
+    .join('');
+  galleryElement.insertAdjacentHTML('beforeend', markup);
+};
+
+const markupGalleryItem = (result, index, isPopular = false) => {
+  //const { title, release_date, poster_path, genre_ids } = result;
+  const { title, release_date, poster_path} = result;
+  const coverUrl = poster_path? `https://image.tmdb.org/t/p/w500${poster_path}`: nullPoster;
+ // const genres = genre_ids ? getGenres(genre_ids, genreList) : 'Unknown';
+  const year = release_date ? release_date.slice(0, 4) : 'N/A';
+
+  return `
+    <li class="gallery__items">
+    <div class="gallery__items__img">
+      <a href="${coverUrl}" data-lightbox="group-${index}">
+        <img src="${coverUrl}" alt="${title}" loading="lazy" />
+      </a>
+    </div>
+    <div class="gallery__items__details">
+      <p class="gallery__items__details--info">
+        <b>${title}</b>
+      </p>
+      <p class="gallery__items__details--info">
+        <b>${year}</b>
+      </p>
+    </div>
+  </li>
+    `;
+};
+const getGalleryElement = () => document.querySelector('.gallery');
+export { handleResponse, markupGalleryItem };
+
+/* <p class="gallery__items__details--info">
+<b>Genres: ${genres.join(', ')}</b>
+</p> */
