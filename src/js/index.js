f4b95db8ed64dba49b95d6929fbb6f199f1b13d4
AndreeaC24 <119935685+AndreeaC24@users.noreply.github.com>
2023-06-20 00:11:01 +0300

    index update

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import { fetchGenreList } from './fetchGenre';
 import { handleResponse, getGalleryElement } from './markup';
 import { fetchPopularMovies } from './fetchmvs';
 import './search'; 
+import './footer'; 
 import { initializeModal } from './modal';
 import { showLoader, hideLoader } from './loader.js';
 import { showPage } from './page.js'; 
@@ -9,7 +10,6 @@ import { getGalleryElement } from './utils';
 
 const currentPage = 1;
 //let searchQuery = '';
-
 //const getGalleryElement = () => document.querySelector('.gallery');
 
 const initialize = async () => {
