a669ce92927f21cd648a93d9dddea42e0f210cea
luminitza24 <118338076+luminitza24@users.noreply.github.com>
2023-06-19 22:18:13 +0000

    Deploying to gh-pages from @ luminitza24/filmoteka-js@4b881ea2ee605bdfd2013d92bf1b42e952da14e4 🚀

--- index.3265a660.js
+++ index.3265a660.js
@@ -1,2 +1,2 @@
 !function(){var e=document.getElementById("checkbox");"true"===localStorage.getItem("darkMode")&&(document.body.classList.add("dark"),e.checked=!0),e.addEventListener("change",function(){document.body.classList.toggle("dark");var e=document.body.classList.contains("dark");localStorage.setItem("darkMode",e.toString())})}();
-//# sourceMappingURL=library.bc519094.js.map
+//# sourceMappingURL=index.3265a660.js.map
