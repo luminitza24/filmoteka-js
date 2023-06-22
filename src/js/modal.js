193a8bab00fe679a13b9197f4f30a6eb173a2e69
AlexandruMoldoveanu95 <111571285+AlexandruMoldoveanu95@users.noreply.github.com>
2023-06-22 22:24:33 +0300

    dcdcd

--- src/js/modal.js
+++ src/js/modal.js
@@ -14,55 +14,49 @@ const createModalContent = movie => {
   const coverUrl = movie.poster_path  ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`: nullPoster;
   //console.log(movie.genre_ids);
   const markup = `
-     
+    
                 <img src="${coverUrl}" alt="${
     movie.title
   }" loading="lazy" class="img_movie" />
-  
-  <ul  class="ul__list"> 
+  <section class="section__modal"> 
   <h1 class="title__modal">${movie.title} </h1>
-           <section class="section__modal">
-           
+            <ul  class="ul__list">
              <li class="gallery__modal">
-                <p class="vote__count"> Vote/Votes 
-                <b class="details">  ${
+                <p class="vote__count">Vote/Votes  <b class="details">${
                   movie.vote_average
-                }</b>
-                <b >/</b>
-                 <b >${
+                }</b>  <b >/</b>
+                 <b>${
                  movie.vote_count
                }</b>
-                </p>
                 </li>
-               
             <li class="gallery__modal">
-            <p class="vote__count > 
-            <b class="details"> popularity ${movie.popularity}</b>
+            <p class="vote__count >
+            <b class="details">popularity:${movie.popularity}</b>
             </p>
             </li>
             <li class="gallery__modal">
             <p class="vote__count>
-            <b class="details">Original Title ${movie.original_title}</b>
+            <b class="details">Original Title:${movie.original_title}</b>
             </p>
             </li>
             <li class="gallery__modal">
-                <p class="vote__count> 
-                 <b class="details">Genre${genres.join(', ')}</b>
+                <p class="vote__count>
+                 <b class="details">Genre:${genres.join(', ')}</b>
                 </p>
             </li>
             <li class="gallery__modal">
             <h3 class="details__info">about</h3>
                 <p class="text__modal">${movie.overview}</p>
             </li>
-           <ul class="button_list">
+        </ul>
+        <ul class="button_list">
             <li>
-            <button class="modal__button__item-queue" type="button">add to watched</button>
+            <button class="modal__button__item-watched" type="button">add to watched</button>
             </li>
             <li>
             <button class="modal__button__item-queue" type="button"> add to queue</button>
             </li>
             </ul>
-        </ul>
         </section>
     `;
   modalContent.innerHTML = markup;
@@ -94,5 +88,11 @@ const initializeModal = () => {
     closeModalBtn.addEventListener('click', toggleModal);
   }
 };
+const modall=document.getElementById('.modal');
+window.addEventListener("keydown", function(event){
+  if(event.key === "Excape"){
+    modall.style.display= "none"
+  };
+});
 
 export { openModal, initializeModal };
